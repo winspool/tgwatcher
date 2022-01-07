6da3403b82037980113ed2c4db8a87e67138b9b5
Detlef Riekenberg <wine.dev@web.de>
2022-01-08 00:19:01 +0100

    Add "Hello World" source with support for autotools and cmake build systems
    --
    Regards --- Detlef

---
+++ src/tgwatcher.c
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2021-22 Detlef Riekenberg
+ * 
+ * You have the option to choose between two licenses:
+ * Select either the MIT License
+ * or the public domain license.
+ *
+ * See the LICENSE file
+ *
+ * newest version:
+ * https://www.github.com/winspool/tgwatcher
+ * 
+ * ############################## */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* ############################## */
+
+
+#include <stdio.h>
+#include <stdint.h>
+
+
+/* ############################## */
+
+int main(int argc, char **argv)
+{
+	printf("%s started\n", __FUNCTION__);
+
+
+	puts("Hello World\n");
+
+
+
+	printf("end of %s\n", __FUNCTION__);
+	return 0;
+}
+
+/* ############################## */
+
+#ifdef __cplusplus
+}
+#endif
+
